786f07361364f59b9fdba22c6b54494d9f32a855
Mayank Saini <mayanksaini0416@gmail.com>
2025-11-01 21:56:10 +0530

    fix: initialize currentStep to 0 in HowToOrderSection

--- src/components/HowToOrderSection.tsx
+++ src/components/HowToOrderSection.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export const HowToOrderSection = () => {
-  const [currentStep, setCurrentStep] = useState(4);
+  const [currentStep, setCurrentStep] = useState(0);
 
   const steps = [
     {
